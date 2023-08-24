2bdb68d41b90f1205bcfbc30e083d3da76f7ed9a
Donovan Barrientos <donovan_ba@outlook.com>
2023-08-24 16:59:59 -0600

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primer App de Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
 }
