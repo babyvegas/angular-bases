22777d35bcc0912526b6432fc54867544e148579
Donovan Barrientos <donovan_ba@outlook.com>
2023-10-17 17:10:32 -0600

    utilizando uuid

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,19 +8,7 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent  {
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name: 'Goku',
-    power: 9500
-  },{
-    name: 'Vegeta',
-    power: 7500
-  }];
+    constructor( public dbzService: DbzService) {
 
-  onNewCharacter( character: Character):void {
-    console.log('main page');
-    console.log(character);
-  }
+    }
 }
